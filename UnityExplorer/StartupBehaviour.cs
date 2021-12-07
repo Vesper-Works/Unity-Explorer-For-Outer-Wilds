e55a9bb7c72a0bdac85b76a76741b5a2b45d904f
Vesper <59376295+Vesper-Works@users.noreply.github.com>
2021-12-07 10:16:56 +0000

    Initial commit

---
+++ UnityExplorer/StartupBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using OWML.ModHelper;
+using OWML.Common;
+namespace UnityExplorer
+{
+    public class StartupBehaviour : ModBehaviour
+    {
+        private void Start()
+        {
+            ExplorerStandalone.CreateInstance();
+        }
+    }
+}
