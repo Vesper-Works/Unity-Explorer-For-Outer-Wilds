a9765e1a1d7b581e51d781190f610a17536e66e3
Locochoco <ivanrwpf@gmail.com>
2022-11-07 13:52:24 -0300

    Added hotkeys for gizmos types and subtypes

--- UnityExplorer/ExtendedTransformTools.cs
+++ UnityExplorer/ExtendedTransformTools.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 using UnityExplorer.GLDrawHelpers;
 using UnityExplorer.Inspectors;
@@ -12,31 +13,109 @@ namespace UnityExplorer
 
         float maxDistanceToSelect = 0.05f;
 
-        LocalPositionGizmo localPositionGizmo = new();
-        LocalEulerAngleGizmo localEulerAngleGizmo = new();
-        FromCameraViewRotationGizmo fromCameraViewRotationGizmo = new();
-        TransformOrientationGizmo transformOrientationGizmo = new();
+
+        private List<BaseTransformGizmo> EnabledGizmos = new();
+
+        int previousSelectedGizmo = 0;
+        int previousSubTypeGizmoOption = 0;
+
+        private void ChangeGizmos(int selectedGizmo, int subTypeGizmoOption) 
+        {
+            selectedGizmo %= 3;
+
+            if (previousSelectedGizmo != selectedGizmo)
+            {
+                EnabledGizmos.Clear();
+            }
+            switch (selectedGizmo) 
+            {
+                case 1:
+                    ChangePositionGizmos(subTypeGizmoOption);
+                    break;
+                case 2:
+                    ChangeRotationGizmos(subTypeGizmoOption);
+                    break;
+
+                case 0:
+                default:
+                    break;
+            }
+
+            previousSelectedGizmo = selectedGizmo;
+        }
+
+        private readonly List<BaseTransformGizmo> PositionGizmos = new()
+        {
+            new LocalPositionGizmo(),
+            new TransformOrientationGizmo(),
+        };
+        private void ChangePositionGizmos(int positionGizmoTypes)
+        {
+            positionGizmoTypes %= PositionGizmos.Count;
+
+            if (previousSubTypeGizmoOption != positionGizmoTypes)
+            {
+                EnabledGizmos.Clear();
+            }
+            if (PositionGizmos.Count > 0)
+            {
+                EnabledGizmos.Add(PositionGizmos[positionGizmoTypes]);
+            }
+
+            previousSubTypeGizmoOption = positionGizmoTypes;
+        }
+
+        private readonly List<BaseTransformGizmo> RotationGizmos = new()
+        {
+            new LocalEulerAngleGizmo(),
+            new FromCameraViewRotationGizmo(),
+        };
+        private void ChangeRotationGizmos(int rotationGizmoTypes)
+        {
+            rotationGizmoTypes %= 2;
+
+            if (previousSubTypeGizmoOption != rotationGizmoTypes)
+            {
+                EnabledGizmos.Clear();
+            }
+            if (RotationGizmos.Count > 0)
+            {
+                EnabledGizmos.Add(PositionGizmos[rotationGizmoTypes]);
+            }
+            previousSubTypeGizmoOption = rotationGizmoTypes;
+        }
+
 
         //TODO add hotkey to enable gizmos
         //And maybe settings for choosing them on the configs
+        private void HotKeysForSwaping() 
+        {
+            if (UniverseLib.Input.InputManager.GetKeyUp(KeyCode.R)) 
+            {
+                ChangeGizmos(previousSelectedGizmo + 1, previousSubTypeGizmoOption);
+            }
+            
+            if (UniverseLib.Input.InputManager.GetKeyUp(KeyCode.T)) 
+            {
+                ChangeGizmos(previousSelectedGizmo, previousSubTypeGizmoOption + 1);
+            }
+            
+        }
+
         private void Update()
         {
+            HotKeysForSwaping();
+
             var inspector = InspectorManager.ActiveInspector;
 
             if (inspector is not GameObjectInspector goInspector)
             {
-                localPositionGizmo.Reset();
-                localEulerAngleGizmo.Reset();
-                transformOrientationGizmo.Reset();
-                fromCameraViewRotationGizmo.Reset();
+                EnabledGizmos.ForEach((gizmo) => gizmo.Reset());
                 return;
             }
             if(goInspector.Target == null)
             {
-                localPositionGizmo.Reset();
-                localEulerAngleGizmo.Reset();
-                transformOrientationGizmo.Reset();
-                fromCameraViewRotationGizmo.Reset();
+                EnabledGizmos.ForEach((gizmo) => gizmo.Reset());
                 return;
             }
 
@@ -44,66 +123,36 @@ namespace UnityExplorer
 
             float scale = Vector3.Distance(Locator.GetActiveCamera().transform.position, selectedTransform.position) / 8f;
 
-            localPositionGizmo.SetScale(scale);
-            localEulerAngleGizmo.SetScale(scale);
-            transformOrientationGizmo.SetScale(scale);
-            fromCameraViewRotationGizmo.SetScale(scale);
-
-            localPositionGizmo.Set(selectedTransform);
-            localEulerAngleGizmo.Set(selectedTransform);
-            transformOrientationGizmo.Set(selectedTransform);
-            fromCameraViewRotationGizmo.Set(selectedTransform);
-
+            EnabledGizmos.ForEach((gizmo) =>
+            {
+                gizmo.SetScale(scale);
+                gizmo.Set(selectedTransform);
+            });
 
             Ray ray = Locator.GetActiveCamera().ScreenPointToRay(UniverseLib.Input.InputManager.MousePosition);
 
             if (UniverseLib.Input.InputManager.GetMouseButtonDown(0))
             {
-                if (!(localPositionGizmo.IsSelected() || localEulerAngleGizmo.IsSelected() || transformOrientationGizmo.IsSelected() || fromCameraViewRotationGizmo.IsSelected()))
+                bool noneIsSelected = EnabledGizmos.TrueForAll((gizmo) => !gizmo.IsSelected());
+
+                if (noneIsSelected)
                 {
-                    
-                    localPositionGizmo.CheckSelected(ray, maxDistanceToSelect * scale);
-                    //TODO create a way to have selection priority (like in this if)
-                    if (!localPositionGizmo.IsSelected())
-                    {
-                        transformOrientationGizmo.CheckSelected(ray, maxDistanceToSelect * scale);
-                    }
-                    //TODO create a way to have incompatible gizmos not being selected at the same time
-                    if (!transformOrientationGizmo.IsSelected())
-                    {
-                        localEulerAngleGizmo.CheckSelected(ray, maxDistanceToSelect * scale);
-                    }
-                    if (!localEulerAngleGizmo.IsSelected())
-                    {
-                        fromCameraViewRotationGizmo.CheckSelected(ray, maxDistanceToSelect * scale);
-                    }
+                    EnabledGizmos.ForEach((gizmo) => gizmo.CheckSelected(ray, maxDistanceToSelect * scale));
                 }
             }
             else if (UniverseLib.Input.InputManager.GetMouseButton(0))
             {
-                if (localPositionGizmo.IsSelected())
+                EnabledGizmos.ForEach((gizmo) =>
                 {
-                    localPositionGizmo.OnSelected(ray);
-                }
-                if (transformOrientationGizmo.IsSelected())
-                {
-                    transformOrientationGizmo.OnSelected(ray);
-                }
-                if (localEulerAngleGizmo.IsSelected())
-                {
-                    localEulerAngleGizmo.OnSelected(ray);
-                }
-                if (fromCameraViewRotationGizmo.IsSelected())
-                {
-                    fromCameraViewRotationGizmo.OnSelected(ray);
-                }
+                    if (gizmo.IsSelected())
+                    {
+                        gizmo.OnSelected(ray);
+                    }
+                });
             }
             else
             {
-                localPositionGizmo.Reset();
-                localEulerAngleGizmo.Reset();
-                transformOrientationGizmo.Reset();
-                fromCameraViewRotationGizmo.Reset();
+                EnabledGizmos.ForEach((gizmo) => gizmo.Reset());
             }
 
         }
@@ -111,14 +160,12 @@ namespace UnityExplorer
         {
             if (selectedTransform == null)
                 return;
+
             GL.wireframe = true;
 
             GLHelper.SetDefaultMaterialPass(0, true);
 
-            localPositionGizmo.OnRender();
-            localEulerAngleGizmo.OnRender();
-            transformOrientationGizmo.OnRender();
-            fromCameraViewRotationGizmo.OnRender();
+            EnabledGizmos.ForEach((gizmo) => gizmo.OnRender());
 
             GL.wireframe = false;
         }
