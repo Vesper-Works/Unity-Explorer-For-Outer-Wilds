450089eb50b6605d05a8d2728c46128a0f61f4fe
JohnCorby <williamforgames6@gmail.com>
2025-01-23 15:25:43 -0800

    disable gizmos

--- UnityExplorer/UnityExplorer.cs
+++ UnityExplorer/UnityExplorer.cs
@@ -32,6 +32,9 @@ namespace UnityExplorer
                     })
             );
 
+            // gizmos seem to cause problems sometimes according to anon, so im just gonna turn em off for now
+            // at some point loco should fix the stuff
+            return;
             gameObject.AddComponent<ExtendedTransformTools>();
         }
     }
